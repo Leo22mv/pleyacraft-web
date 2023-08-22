f1f60195388679c9a6beaf4e178b72d86a6a3e2c
Kbza <g.leonel.m17@gmail.com>
2023-08-22 10:01:27 -0300

    1.0

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,11 @@ function App() {
       <h1>PLEYACRAFT</h1>
       {/* <h3>El mod donde el cielo no es un límite</h3> */}
       <Items />
+      <div className='description'>
+      <p>Pleyacraft es un mod para Minecraft forge 1.19, que añade un nuevo mineral llamado "Pleyarita", generado de forma natural en el mundo y que al picarlo con un pico de hierro o mejor soltará un lingote de pleyarita, con el cual podrás construir una espada o cualquiera de las otras herramientas usando las recetas tradicionales. Estas herramientas son igual de efectivas y duraderas que las de netherita, excepto por la espada, siendo mas dañina la de pleyarita.</p>
+      </div>
       <a href="../assets/pleyacraft-1.0.jar" download="pleyacraft-1.0.jar">
-        <button>Descargar</button>
+        <button className='btn btn-dark btn-lg'>Descargar</button>
       </a>
       <br />
       <a href='https://leomartinez.dev.ar' target='_blank' className='credits'>Desarrollado por LM Software</a>
